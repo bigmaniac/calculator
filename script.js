c6b331b54fb2e8d4c2ce90d84519150c2beccb06
bigmaniac <1matik.m@gmail.com>
2025-05-22 23:41:12 +0200

    Modify script.js
    - add 0 division error

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 function add(arg1, arg2) {
-    return result = arg1 + arg2;
+    result = parseInt(arg1) + parseInt(arg2);
+    display.innerHTML = result;
 };
 
 function subtract(arg1, arg2) {
-    return result = arg1 - arg2;
+    result = arg1 - arg2;
+    display.innerHTML = result;
 };
 
 function multiply(arg1, arg2) {
-    return result = arg1 * arg2;
+    result = arg1 * arg2;
+    display.innerHTML = result;
 };
 
 function divide(arg1, arg2) {
-    if (arg2 === 0) {
-        return result = "error: 0 division";
+    if (arg2 === "0") {
+        result = "err: 0 division";
+    } else {
+        result = arg1 / arg2;
     };
-    return result = arg1 / arg2;
+    display.innerHTML = result;
 };
 
 let firstNumber = "";
@@ -35,11 +40,9 @@ btnNumber.forEach(btn => {
     btn.addEventListener("click", () => {
         if (operation === "") {
             firstNumber += btn.innerHTML;
-            // console.log(firstNumber);
             display.innerHTML = firstNumber;
         } else {
             secondNumber += btn.innerHTML;
-            // console.log(secondNumber);
             display.innerHTML = secondNumber;
         };
     });
@@ -50,8 +53,27 @@ btnOperator.forEach(btn => {
     btn.addEventListener("click", () => {
         if (secondNumber === "") {
             operation = btn.innerHTML;
-            // console.log(operation);
             display.innerHTML = operation;
         };
     });
+});
+
+function operate(number1, number2, operator) {
+    if (operator === "+") {
+        add(number1, number2);
+    }
+    else if (operator === "-") {
+        subtract(number1, number2);
+    }
+    else if (operator === "x") {
+        multiply(number1, number2);
+    }
+    else if (operator === "/") {
+        divide(number1, number2);
+    };
+};
+
+const btnEquals = document.querySelector("#equals");
+btnEquals.addEventListener("click", () => {
+    operate(firstNumber, secondNumber, operation)
 });
